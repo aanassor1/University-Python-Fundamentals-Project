c130d5d0f8a7cbe316c8cae7fcd87ab000f84f71
aaziz1234 <aan15@student.le.ac.uk>
2023-07-29 15:14:03 +0100

    Remove redundant code

--- Task6.py
+++ Task6.py
@@ -1,22 +1,8 @@
-ScoresFile = open(r"D:\Documents\Uni\Year 1\Programming\Assignment\scores.txt", "r") #open the score file
-ScoresDictionary = {} #the score dictionary
-for x in ScoresFile: #Read the entire file line by line and then add the line into the corresponding list.
-  score_letter = x.split()[0].lower() #The score letter
-  score_score = x.split()[1] #The score of the letter
-  ScoresDictionary[score_letter] = score_score #Input it in the dictionary
-ScoresFile.close() #Close the score file
-
 DictionaryFile = open(r"D:\Documents\Uni\Year 1\Programming\Assignment\dictionary.txt", "r") #open the dictionary file
 DictionaryList = [] #the dictionary list
 for x in DictionaryFile: #Read the entire file line by line and then add the line into the corresponding list.
     DictionaryList.append(x.upper()[0:len(x)-1])
 DictionaryFile.close() #Close the dictionary file
-
-def getLetterScore(Letter): #Return the score of the letter
-    if len(Letter) == 1: #Make sure the letter is only length 1
-        is_letter = Letter.isalpha()
-        if is_letter == True: #Check if its a letter
-            return ScoresDictionary.get(Letter.lower()) #Return the score of the letter
 
 def checkWord(Word, dictionary): #Check if the word is in the dictionary
     if Word.isalpha() == True:  # Check if its a word
